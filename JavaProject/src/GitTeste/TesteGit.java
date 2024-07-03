20498305d9d3299aa667876236dfe439ccf71241
EduardoHHemer <eduardohhemer@gmail.com>
2024-07-03 15:09:04 -0300

    Update TesteGit.java

--- JavaProject/src/GitTeste/TesteGit.java
+++ JavaProject/src/GitTeste/TesteGit.java
@@ -5,6 +5,7 @@ public class TesteGit {
 	public static void main(String[] args) {
 	
 		System.out.println("Integração Git Eclipse Primeiro Teste");
+		System.out.println("Alterando do git para o eclipse");
 	}
 
 }
